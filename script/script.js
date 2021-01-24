d6904f6fa214a1ceaf17f358caff0a0a82c4d1f0
wawanneutron <wawanneutron1331@gmail.com>
2021-01-24 15:47:34 +0700

    update

--- script/script.js
+++ script/script.js
@@ -8,6 +8,12 @@ $(document).ready(function(){
         }
     })
 
+    // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#mainNav',
+    offset: 75
+  });
+
     
 })
 
